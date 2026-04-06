a7ee93149d8bd87575305415c0482e8d50a55506
badluevdima5 <badluevdima5@gmail.com>
2026-04-06 23:52:11 +0700

    добавил Room Entity и DAO

---
+++ app/src/main/java/ru/nsu/badluev/memo/data/NoteDao.kt
@@ -0,0 +1,17 @@
+package ru.nsu.badluev.memo.data
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY createdAt DESC")
+    fun getAllNotes(): Flow<List<NoteEntity>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: NoteEntity)
+
+    @Delete
+    suspend fun deleteNote(note: NoteEntity)
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    suspend fun getNoteById(id: Int): NoteEntity?
+}
