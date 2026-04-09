82105b53b17c1de49b5472ce4f7d1212d08ae379
badluevdima5 <badluevdima5@gmail.com>
2026-04-10 00:11:14 +0700

    добавил первый экран с заголовком

--- app/src/main/java/ru/nsu/badluev/memo/MainActivity.kt
+++ app/src/main/java/ru/nsu/badluev/memo/MainActivity.kt
@@ -1,51 +1,29 @@
 package ru.nsu.badluev.memo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.nsu.badluev.memo.ui.theme.MemoTheme
 
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    StartScreen()
-                }
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {TopAppBar(title = {Text("Мои Заметки")})}) {}
             }
         }
-    }
-}
-
-@Composable
-fun StartScreen(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Мои заметки",
-            style = MaterialTheme.typography.headlineMedium
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun StartScreenPreview() {
-    MemoTheme {
-        StartScreen()
     }
 }
