2be952849774ea038f6e9ac8bce4d3532bf1a7bc
badluevdima5 <badluevdima5@gmail.com>
2026-04-10 00:27:31 +0700

    добавил кнопку "+" - добавить заметку

--- app/src/main/java/ru/nsu/badluev/memo/MainActivity.kt
+++ app/src/main/java/ru/nsu/badluev/memo/MainActivity.kt
@@ -2,15 +2,21 @@ package ru.nsu.badluev.memo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import ru.nsu.badluev.memo.ui.theme.MemoTheme
 
 
@@ -22,7 +28,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {TopAppBar(title = {Text("Мои Заметки")})}) {}
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {TopAppBar(title = {Text("Мои заметки")})},
+                    floatingActionButton = {FloatingActionButton(onClick = { }) {Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Добавить заметку"
+                    )}}
+                ) {}
             }
         }
     }
